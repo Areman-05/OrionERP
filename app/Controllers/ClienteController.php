5ee6641c391be3c181388429dd6e9cae371e7b17
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-11-19 09:37:58 +0100

    Crear ClienteController con metodos index y show

---
+++ app/Controllers/ClienteController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OrionERP\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use OrionERP\Models\Cliente;
+
+class ClienteController
+{
+    private $clienteModel;
+
+    public function __construct()
+    {
+        $this->clienteModel = new Cliente();
+    }
+
+    public function index(Request $request, Response $response): Response
+    {
+        $clientes = $this->clienteModel->getAll();
+        
+        $response->getBody()->write(json_encode([
+            'success' => true,
+            'data' => $clientes
+        ]));
+        
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    public function show(Request $request, Response $response, array $args): Response
+    {
+        $id = (int) $args['id'];
+        $cliente = $this->clienteModel->findById($id);
+        
+        if (!$cliente) {
+            $response->getBody()->write(json_encode([
+                'success' => false,
+                'message' => 'Cliente no encontrado'
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+        }
+        
+        $response->getBody()->write(json_encode([
+            'success' => true,
+            'data' => $cliente
+        ]));
+        
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
+
