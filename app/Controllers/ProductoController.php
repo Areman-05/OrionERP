808f2a92b7e7783f1d7b4330283670561f912c7b
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-11-19 09:38:11 +0100

    Crear ProductoController con metodos index y show

---
+++ app/Controllers/ProductoController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OrionERP\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use OrionERP\Models\Producto;
+
+class ProductoController
+{
+    private $productoModel;
+
+    public function __construct()
+    {
+        $this->productoModel = new Producto();
+    }
+
+    public function index(Request $request, Response $response): Response
+    {
+        $productos = $this->productoModel->getAll();
+        
+        $response->getBody()->write(json_encode([
+            'success' => true,
+            'data' => $productos
+        ]));
+        
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    public function show(Request $request, Response $response, array $args): Response
+    {
+        $id = (int) $args['id'];
+        $producto = $this->productoModel->findById($id);
+        
+        if (!$producto) {
+            $response->getBody()->write(json_encode([
+                'success' => false,
+                'message' => 'Producto no encontrado'
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+        }
+        
+        $response->getBody()->write(json_encode([
+            'success' => true,
+            'data' => $producto
+        ]));
+        
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
+
