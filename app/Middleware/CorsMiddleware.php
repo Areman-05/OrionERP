e9560f49358688956cae8363a5d6f77cb859a30a
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-11-30 19:45:22 +0100

    Agregar middleware CORS configurable

---
+++ app/Middleware/CorsMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OrionERP\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CorsMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        
+        $allowedOrigins = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '*';
+        
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', $allowedOrigins)
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
+    }
+}
+
