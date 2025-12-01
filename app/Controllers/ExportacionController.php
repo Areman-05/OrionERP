6ddfb71f427c249c72b3191cff5828e6299194a4
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-01 20:02:03 +0100

    Mejorar controlador de exportaciones con Excel

--- app/Controllers/ExportacionController.php
+++ app/Controllers/ExportacionController.php
@@ -5,19 +5,22 @@ namespace OrionERP\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use OrionERP\Services\ExportacionService;
+use OrionERP\Services\ExportacionExcelService;
 
 class ExportacionController
 {
     private $exportacionService;
+    private $excelService;
 
     public function __construct()
     {
         $this->exportacionService = new ExportacionService();
+        $this->excelService = new ExportacionExcelService();
     }
 
-    public function exportarProductos(Request $request, Response $response): Response
+    public function exportarProductosExcel(Request $request, Response $response): Response
     {
-        $csv = $this->exportacionService->exportarProductos();
+        $csv = $this->excelService->exportarProductosExcel();
         
         $response->getBody()->write($csv);
         return $response
@@ -25,23 +28,13 @@ class ExportacionController
             ->withHeader('Content-Disposition', 'attachment; filename="productos_' . date('Y-m-d') . '.csv"');
     }
 
-    public function exportarClientes(Request $request, Response $response): Response
-    {
-        $csv = $this->exportacionService->exportarClientes();
-        
-        $response->getBody()->write($csv);
-        return $response
-            ->withHeader('Content-Type', 'text/csv; charset=utf-8')
-            ->withHeader('Content-Disposition', 'attachment; filename="clientes_' . date('Y-m-d') . '.csv"');
-    }
-
-    public function exportarVentas(Request $request, Response $response): Response
+    public function exportarVentasExcel(Request $request, Response $response): Response
     {
         $queryParams = $request->getQueryParams();
         $fechaInicio = $queryParams['fecha_inicio'] ?? date('Y-m-01');
         $fechaFin = $queryParams['fecha_fin'] ?? date('Y-m-d');
         
-        $csv = $this->exportacionService->exportarVentas($fechaInicio, $fechaFin);
+        $csv = $this->excelService->exportarVentasExcel($fechaInicio, $fechaFin);
         
         $response->getBody()->write($csv);
         return $response
@@ -49,4 +42,3 @@ class ExportacionController
             ->withHeader('Content-Disposition', 'attachment; filename="ventas_' . date('Y-m-d') . '.csv"');
     }
 }
-
