e4333cdd6eae0c11f5bfea27fe8a660694c7d78c
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-01 19:59:57 +0100

    Agregar utilidades de fecha

--- app/Utils/DateHelper.php
+++ app/Utils/DateHelper.php
@@ -4,20 +4,50 @@ namespace OrionERP\Utils;
 
 class DateHelper
 {
-    public static function now(): string
+    public static function formatearFecha(string $fecha, string $formato = 'd/m/Y'): string
     {
-        return date('Y-m-d H:i:s');
+        $date = new \DateTime($fecha);
+        return $date->format($formato);
     }
 
-    public static function format(string $date, string $format = 'Y-m-d'): string
+    public static function formatearFechaHora(string $fecha, string $formato = 'd/m/Y H:i'): string
     {
-        return date($format, strtotime($date));
+        $date = new \DateTime($fecha);
+        return $date->format($formato);
     }
 
-    public static function isValid(string $date): bool
+    public static function esFechaValida(string $fecha, string $formato = 'Y-m-d'): bool
     {
-        $d = \DateTime::createFromFormat('Y-m-d', $date);
-        return $d && $d->format('Y-m-d') === $date;
+        $date = \DateTime::createFromFormat($formato, $fecha);
+        return $date && $date->format($formato) === $fecha;
+    }
+
+    public static function agregarDias(string $fecha, int $dias): string
+    {
+        $date = new \DateTime($fecha);
+        $date->modify("+$dias days");
+        return $date->format('Y-m-d');
+    }
+
+    public static function diferenciaDias(string $fecha1, string $fecha2): int
+    {
+        $date1 = new \DateTime($fecha1);
+        $date2 = new \DateTime($fecha2);
+        $diff = $date1->diff($date2);
+        return (int) $diff->format('%r%a');
+    }
+
+    public static function getPrimerDiaMes(string $fecha = 'now'): string
+    {
+        $date = new \DateTime($fecha);
+        $date->modify('first day of this month');
+        return $date->format('Y-m-d');
+    }
+
+    public static function getUltimoDiaMes(string $fecha = 'now'): string
+    {
+        $date = new \DateTime($fecha);
+        $date->modify('last day of this month');
+        return $date->format('Y-m-d');
     }
 }
-
