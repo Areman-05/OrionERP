e7bef5005abafd3779777e1a3be0c6c7e45f78d6
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-01 20:00:35 +0100

    Agregar middleware de configuracion

---
+++ app/Middleware/ConfigMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OrionERP\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use OrionERP\Services\ConfiguracionService;
+
+class ConfigMiddleware implements MiddlewareInterface
+{
+    private $configService;
+
+    public function __construct()
+    {
+        $this->configService = new ConfiguracionService();
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // Agregar configuración a los atributos de la request
+        $request = $request->withAttribute('config', $this->configService->getConfiguracionCompleta());
+        $request = $request->withAttribute('empresa', $this->configService->getDatosEmpresa());
+        
+        return $handler->handle($request);
+    }
+}
+
